a8c62774e5086d56166c25363b1d7235ccfb891b
PC <selinz.bolukbas@gmail.com>
2025-07-21 16:52:03 +0300

    [JA-17] - Hibernate Delete İşlemi

--- hibernate/src/main/java/com/selin/Main.java
+++ hibernate/src/main/java/com/selin/Main.java
@@ -2,9 +2,13 @@ package com.selin;
 
 
 import com.selin.model.Student;
+import com.selin.model.User;
 import com.selin.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,16 +18,43 @@ public class Main {
         try {
             tx = session.beginTransaction();
 
-            Student student = new Student("Selin",26);
-            session.save(student);
+           // Student student = new Student("Selin",26);
+           // session.save(student);
 
-            Student readStudent=session.get(Student.class, student.getId());
-            System.out.println(readStudent);
+            //Student readStudent=session.get(Student.class, student.getId());
+           // System.out.println(readStudent);
+
+            // create
+           /* User user=new User();
+            user.setEmail("selin@gmail.com");
+            user.setName("Selin");
+
+            session.save(user);*/
+
+
+            // read
+            User readUser=session.get(User.class, 2);
+            System.out.println(readUser);
+            List<User> users =  session.createQuery("from User", User.class).list();
+
+            for (User user : users) {
+                System.out.println(user);
+            }
+
+            // update
+            readUser.setName("Sude");
+            readUser.setEmail("sude@gmail.com");
+            session.update(readUser);
+
+            // delete
+            session.delete(readUser);
 
             tx.commit();
         }catch (Exception e) {
             if(tx!=null) tx.rollback();
             e.printStackTrace();
+        }finally {
+            session.close();
         }
 
     }
